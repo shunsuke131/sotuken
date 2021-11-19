d85dc67dfbb2ed50dfa6547abe0d5fc18f793ce5
sato shunsuke <s.satou.sys20@morijyobi.ac.jp>
2021-11-19 09:36:15 +0900

    gitうまくいかなかったため二回目のcommit

---
+++ js/Mouseover.js
@@ -0,0 +1,12 @@
+
+//マウスオーバー時の処理
+function mouseOn(){
+  var obj = document.getElementById("img");
+  obj.src = "../sotuken/img/iOS の画像 (14).jpg";
+}
+ 
+//マウスアウト時の処理
+function mouseOff(){
+  var obj = document.getElementById("img");
+  obj.src = "../sotuken/img/iOS の画像 (15).jpg";
+}
